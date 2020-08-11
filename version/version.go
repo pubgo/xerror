060c92cb2e1e55ff757dffc33af2935ebe7b4e4a
白云辉 <baiyh@weipaitang.com>
2020-08-11 21:42:01 +0800

    fix: add Fmt method

--- version/version.go
+++ version/version.go
@@ -1,6 +1,5 @@
 package version
 
 var GoVersion = "go version go1.14.4 darwin/amd64"
-var CommitID = "18f7fd48"
-var BuildTime = "2020-08-11 21:18:49"
+var BuildTime = "2020-08-11 21:41:35"
 var Version = "v0.2.7"
