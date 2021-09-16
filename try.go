c4c385a3b5a66a9bb36b99f666faaec9cc486517
白云辉 <kooksee@163.com>
2021-09-16 16:17:52 +0800

    fix: add try

---
+++ try.go
@@ -0,0 +1,29 @@
+package xerror
+
+func TryCatch(fn func(), catch ...func(err error)) {
+	Assert(fn == nil, "[fn] should not be nil")
+
+	if len(catch) > 0 && catch[0] != nil {
+		defer Resp(func(err XErr) { catch[0](err) })
+	}
+
+	fn()
+}
+
+func TryWith(err *error, fn func()) {
+	Assert(fn == nil, "[fn] should not be nil")
+
+	defer RespErr(err)
+	fn()
+
+	return
+}
+
+func Try(fn func()) (err error) {
+	Assert(fn == nil, "[fn] should not be nil")
+
+	defer RespErr(&err)
+
+	fn()
+	return
+}
