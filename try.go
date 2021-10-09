7f96e7118ebe8fe5740f719030d7705580fbc6d9
白云辉 <kooksee@163.com>
2021-10-10 06:02:38 +0800

    fix: try val

--- try.go
+++ try.go
@@ -2,11 +2,14 @@ package xerror
 
 import (
 	"fmt"
+
 	"github.com/pubgo/xerror/internal/utils"
 )
 
 func TryCatch(fn func(), catch ...func(err error)) {
-	Assert(fn == nil, "[fn] should not be nil")
+	if fn == nil {
+		panic("[fn] should not be nil")
+	}
 
 	if len(catch) > 0 && catch[0] != nil {
 		defer Resp(func(err XErr) { catch[0](err) })
@@ -16,7 +19,9 @@ func TryCatch(fn func(), catch ...func(err error)) {
 }
 
 func TryWith(err *error, fn func()) {
-	Assert(fn == nil, "[fn] should not be nil")
+	if fn == nil {
+		panic("[fn] should not be nil")
+	}
 
 	defer RespErr(err)
 	fn()
@@ -24,8 +29,38 @@ func TryWith(err *error, fn func()) {
 	return
 }
 
+func TryVal(fn func() interface{}) interface{} {
+	if fn == nil {
+		panic("[fn] should not be nil")
+	}
+
+	defer func() {
+		val := recover()
+		if val == nil {
+			return
+		}
+
+		var err error
+		handleRecover(&err, val)
+		if isErrNil(err) {
+			return
+		}
+
+		err1 := &xerror{Err: err}
+		err1.Caller = [2]string{
+			utils.CallerWithFunc(fn),
+			utils.CallerWithDepth(4),
+		}
+		panic(err1)
+	}()
+
+	return fn()
+}
+
 func TryThrow(fn func(), args ...interface{}) {
-	Assert(fn == nil, "[fn] should not be nil")
+	if fn == nil {
+		panic("[fn] should not be nil")
+	}
 
 	defer func() {
 		val := recover()
@@ -41,7 +76,10 @@ func TryThrow(fn func(), args ...interface{}) {
 
 		err1 := &xerror{Err: err}
 		err1.Msg = fmt.Sprint(args...)
-		err1.Caller = [2]string{utils.CallerWithFunc(fn)}
+		err1.Caller = [2]string{
+			utils.CallerWithFunc(fn),
+			utils.CallerWithDepth(4),
+		}
 		panic(err1)
 	}()
 
@@ -51,7 +89,9 @@ func TryThrow(fn func(), args ...interface{}) {
 }
 
 func Try(fn func()) (err error) {
-	Assert(fn == nil, "[fn] should not be nil")
+	if fn == nil {
+		panic("[fn] should not be nil")
+	}
 
 	defer RespErr(&err)
 
--- try_test.go
+++ try_test.go
@@ -1,11 +1,34 @@
 package xerror
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestTryThrow(t *testing.T) {
 	defer RespTest(t)
 
 	TryThrow(func() {
 		panic("abc")
-	},"test try throw")
+	}, "test try throw")
+}
+
+func TestTryVal(t *testing.T) {
+	defer RespTest(t)
+
+	fmt.Println(TryVal(func() interface{} {
+		return "hello"
+	}))
+
+	var a = func() {
+		panic("hello")
+	}
+
+	fmt.Println(TryVal(func() interface{} {
+		fmt.Println("hello")
+
+		a()
+		return nil
+	}))
+
 }
