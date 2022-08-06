7333db3784764f7209bf8d4accc3badac9f933c4
百里(barry) <kooksee@163.com>
2022-08-06 18:17:40 +0800

    fix: update xerr

--- errors/errors.go
+++ errors/errors.go
@@ -2,10 +2,13 @@ package errors
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/pubgo/funk/xerr"
 )
 
+type XError = xerr.XError
+type XErr = xerr.XErr
 type Err = xerr.Err
 
 func Is(err, target error) bool {
@@ -18,6 +21,34 @@ func As(err error, target any) bool {
 
 func New(format string, a ...interface{}) xerr.XErr {
 	return xerr.New(format, a...)
+}
+
+func Wrap(err error, args ...interface{}) error {
+	if err == nil {
+		return nil
+	}
+
+	return xerr.WrapXErr(err, func(err *xerr.XError) { err.Detail = fmt.Sprint(args...) })
+}
+
+func WrapFn(err error, fn func(err xerr.XErr) xerr.XErr) error {
+	if err == nil {
+		return nil
+	}
+
+	return fn(xerr.WrapXErr(err))
+}
+
+func WrapF(err error, msg string, args ...interface{}) error {
+	if err == nil {
+		return nil
+	}
+
+	return xerr.WrapXErr(err, func(err *xerr.XError) { err.Detail = fmt.Sprintf(msg, args...) })
+}
+
+func ParseXErr(err error, fns ...func(err *xerr.XError)) *xerr.XError {
+	return xerr.WrapXErr(err, fns...)
 }
 
 func Unwrap(err error) error {
